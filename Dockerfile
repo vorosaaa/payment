6597e5e17cdf7ae99422281e90a2a73f1edb8736
Attila Voros <attila.voros@lhsystems.com>
2025-02-02 13:56:35 +0100

    Added Swagger (wip), fixed application.yaml and compose.yaml

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
-FROM openjdk:17
+# Stage 1: Build the JAR file
+FROM maven:3.9.9-eclipse-temurin-21 AS build
 WORKDIR /app
-COPY target/*.jar payment-api.jar
+COPY pom.xml .
+COPY src ./src
+RUN mvn clean install
+
+# Stage 2: Create the Docker image
+FROM eclipse-temurin:21
+WORKDIR /app
+COPY --from=build /app/target/*.jar payment-api.jar
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "payment-api.jar"]
--- src/main/java/com/vorosati/payment/project/component/account/AccountRepository.java
+++ src/main/java/com/vorosati/payment/project/component/account/AccountRepository.java
@@ -3,6 +3,7 @@ package com.vorosati.payment.project.component.account;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
@@ -11,6 +12,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT a FROM Account a WHERE a.id = :id")
     Optional<Account> findByIdForUpdate(@Param("id") Long id);
 
 }
